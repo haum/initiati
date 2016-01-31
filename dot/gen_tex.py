612292587a4c7812b7156e2fef16c2b4c7f3e311
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2016-01-31 18:14:13 +0100

    Add Python script to generate the LaTeX map

---
+++ dot/gen_tex.py
@@ -0,0 +1,54 @@
+CG="CG"
+CR="CR"
+CB="CB"
+
+DOORS = [
+	[CG, CR, CG, CB, CB, CB, CB, CR], # E left and right
+	  [CB, CB, CR, CR, CG, CR, CG, CB], # E bottom
+	[CG, CG, CB, CG, CR, CR, CB, CG], # D left and right
+	  [CR, CR, CB, CB, CG, CG, CR, CR], # D bottom
+	[CG, CB, CG, CG, CR, CB, CR, CB], # C left and right
+	  [CG, CG, CR, CG, CB, CG, CG, CG], # C bottom
+	[CR, CB, CB, CG, CR, CR, CG, CR], # B left and right
+	  [CR, CG, CB, CB, CB, CR, CB, CB], # B bottom
+]
+
+
+HEADER = """\\documentclass[a3paper]{article}
+\\usepackage{tikz}
+\\newcommand\CG[2]{\\fill[green] (-#2:#1) circle (.1);}
+\\newcommand\CR[2]{\\fill[red] (-#2:#1) circle (.1);}
+\\newcommand\CB[2]{\\fill[blue] (-#2:#1) circle (.1);}
+\\usepackage[top=0.5cm,bottom=0.5cm,left=0.5cm,right=0.5cm]{geometry}
+\\begin{document}
+\\begin{figure}
+\\centering
+\\begin{tikzpicture}[scale=1.5]
+"""
+
+FOOTER = """\\end{tikzpicture}
+\\end{figure}
+\\end{document}
+"""
+
+print(HEADER)
+
+
+for i in range(len(DOORS[0])):
+    print('\\draw[very thick, black!50] ('+str(-i*360/len(DOORS[0]))+':1) -- ++('+str(-i*360/len(DOORS[0]))+':'+str(len(DOORS))+');')
+
+radius = 0
+angle_step = 45
+for n,ring in enumerate(reversed(DOORS)):
+    radius += 1
+    if n%2==0: # bottom ring
+        angle_shift = 22.5
+        print('\\draw[black!50, very thick] (0,0) circle ('+str(radius)+');')
+    else:
+        angle_shift = 0
+    for i,dottype in enumerate(ring):
+        print("\\"+dottype+"{"+str(radius)+"}{"+str(angle_shift+angle_step*i)+"};")
+
+print('\\draw[black!50, very thick] (0,0) circle ('+str(len(DOORS)+1)+');')
+
+print(FOOTER)
