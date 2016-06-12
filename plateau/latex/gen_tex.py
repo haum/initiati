b64fc32fb4636bafe97594eb9118b0580675e85a
JackDesBwa <jackdesbwa@gmail.com>
2016-06-12 19:42:38 +0200

    update and move plateau in LaTeX

--- plateau/latex/gen_tex.py
+++ plateau/latex/gen_tex.py
@@ -3,13 +3,14 @@ CR="CR"
 CB="CB"
 
 DOORS = [
-	[CG, CR, CG, CB, CB, CB, CB, CR], # E left and right
+	  [CB, CR, CR, CB, CG, CB, CR, CG], # outter ring
+	[CG, CR, CG, CB, CB, CB, CB, CR],   # E left and right
 	  [CB, CB, CR, CR, CG, CR, CG, CB], # E bottom
-	[CG, CG, CB, CG, CR, CR, CB, CG], # D left and right
+	[CG, CG, CB, CG, CR, CR, CB, CG],   # D left and right
 	  [CR, CR, CB, CB, CG, CG, CR, CR], # D bottom
-	[CG, CB, CG, CG, CR, CB, CR, CB], # C left and right
+	[CG, CB, CG, CG, CR, CB, CR, CB],   # C left and right
 	  [CG, CG, CR, CG, CB, CG, CG, CG], # C bottom
-	[CR, CB, CB, CG, CR, CR, CG, CR], # B left and right
+	[CR, CB, CB, CG, CR, CR, CG, CR],   # B left and right
 	  [CR, CG, CB, CB, CB, CR, CB, CB], # B bottom
 ]
 
@@ -35,7 +36,7 @@ print(HEADER)
 
 
 for i in range(len(DOORS[0])):
-    print('\\draw[very thick, black!50] ('+str(-i*360/len(DOORS[0]))+':1) -- ++('+str(-i*360/len(DOORS[0]))+':'+str(len(DOORS))+');')
+    print('\\draw[very thick, black!50] ('+str(-i*360/len(DOORS[0]))+':1) -- ++('+str(-i*360/len(DOORS[0]))+':'+str(len(DOORS)&~1)+');')
 
 radius = 0
 angle_step = 45
@@ -49,6 +50,7 @@ for n,ring in enumerate(reversed(DOORS)):
     for i,dottype in enumerate(ring):
         print("\\"+dottype+"{"+str(radius)+"}{"+str(angle_shift+angle_step*i)+"};")
 
-print('\\draw[black!50, very thick] (0,0) circle ('+str(len(DOORS)+1)+');')
+if len(DOORS)%2 == 0:
+    print('\\draw[black!50, very thick] (0,0) circle ('+str(len(DOORS)+1)+');')
 
 print(FOOTER)
